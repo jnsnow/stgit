df6f97b17832314759376da50eaeaa53c1d5500c
John Snow <jsnow@redhat.com>
2019-08-15 17:04:12 -0400

    setup.py: include lib.git submodule
    stgit.lib.git doesn't appear to be included otherwise.
    
    While we're here, delete a list(map(str, [...])) invocation that
    seemingly does nothing.

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
     description='Stacked GIT',
     long_description='Push/pop utility on top of GIT',
     scripts=['stg'],
-    packages=list(map(str, ['stgit', 'stgit.commands', 'stgit.lib'])),
+    packages=['stgit', 'stgit.commands', 'stgit.lib', 'stgit.lib.git'],
     data_files=[
         ('share/stgit/templates', glob('stgit/templates/*.tmpl')),
         ('share/stgit/examples', glob('examples/*.tmpl')),
